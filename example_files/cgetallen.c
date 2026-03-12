a3ca9154703a2e62cc39d386aa0da48dc2164020
dhoubrechts <dominique.houbrechts@gmail.com>
2026-03-12 22:46:00 +0100

    Add initial decompiled file for cgetallen

---
+++ example_files/cgetallen.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int getal;
+    printf("Voer een getal in: ");
+    scanf("%d", &getal);
+    printf("Getal is: %d\n", getal);
+    float ander_getal;
+    printf("Voer een ander getal in: ");
+    scanf("%f", &ander_getal);
+    printf("Ander getal is: %f\n", ander_getal);
+    printf("Hello, world!");
+    return 0;
+}
+  
